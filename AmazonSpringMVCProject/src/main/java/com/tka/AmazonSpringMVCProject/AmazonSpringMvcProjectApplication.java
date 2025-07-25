e4f77e6755bf1377211b097065631e5a7aa41d6b
Adnan Shaikh <adnanshaikh31001@gmail.com>
2025-07-25 18:20:19 +0530

    Initial commit

---
+++ AmazonSpringMVCProject/src/main/java/com/tka/AmazonSpringMVCProject/AmazonSpringMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tka.AmazonSpringMVCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonSpringMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonSpringMvcProjectApplication.class, args);
+	}
+
+}
---
+++ AmazonSpringMVCProject/src/test/java/com/tka/AmazonSpringMVCProject/AmazonSpringMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tka.AmazonSpringMVCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonSpringMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
